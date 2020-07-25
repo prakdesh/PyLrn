6629cd42621336093bf1e880466d1398ac565f6c
prakdesh <prakdesh@yahoo.com>
2020-07-25 12:25:03 +0530

    This is a code with comments

---
+++ HelloWorld.py
@@ -0,0 +1,11 @@
+"""
+This is a doc string.. Not omitted by interpreter
+"""
+# This is my first code snippet
+# Intent is to learn python step by step
+print("Hello World")
+"""
+This is a document
+string
+"""
+
